8cd313de2dace3521bb751d52d8b170349ac5de7
Tautvydas Venckus <taut.venckus@gmail.com>
2017-10-14 21:48:06 +0300

    Updated README and extended autoload representation.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# hwphpbasics
-Homework: PHP "Basics"
+# Homework: PHP "Basics"
+
+Magic metodai atvaizduojami index.php faile: <br />
+`php index.php`
+<br />
+<br />
+Klasių autoloadinimą galima peržiūrėti autoload-fruit.php faile: <br />
+`php autoload-fruit.php`
--- autoload-fruit.php
+++ autoload-fruit.php
@@ -2,13 +2,14 @@
 
 
 namespace Fruits;
-
+use Fruits\Berries\Grapes\Green;
 use function Fruits\Berries\EggPlants\getBerrie as func;
 
 require "vendor/autoload.php";
 
 echo $eggplants = new Berries\EggPlants\Green();
 echo $green_grapes = new Berries\Grapes\Green();
+echo $green_grapes2 = new Green();
 echo $white_grapes = new Berries\Grapes\White();
 
 echo Berries\Grapes\White::staticWhiteGrape();
